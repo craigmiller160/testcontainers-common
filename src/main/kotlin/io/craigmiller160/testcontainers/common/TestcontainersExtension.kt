d6253e4b65795392ebb2cf16b378f6f5f38487c0
Craig Miller <craigmiller160@gmail.com>
2023-01-20 16:11:14 -0500

    Setting up extension

---
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/TestcontainersExtension.kt
@@ -0,0 +1,13 @@
+package io.craigmiller160.testcontainers.common
+
+import io.craigmiller160.testcontainers.common.config.DefaultConfigResolver
+import org.junit.jupiter.api.extension.BeforeAllCallback
+import org.junit.jupiter.api.extension.ExtensionContext
+
+class TestcontainersExtension : BeforeAllCallback {
+  private val resolver = DefaultConfigResolver()
+  override fun beforeAll(context: ExtensionContext) {
+    val config = resolver.resolve()
+    TODO("Not yet implemented")
+  }
+}
