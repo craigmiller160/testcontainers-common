17828112499118083a88136485e10e8376b20887
Craig Miller <craigmiller160@gmail.com>
2023-01-20 15:48:52 -0500

    Stubbed out test

---
+++ src/test/kotlin/io/craigmiller160/testcontainers/common/config/DefaultConfigResolverTest.kt
@@ -0,0 +1,10 @@
+package io.craigmiller160.testcontainers.common.config
+
+import org.junit.jupiter.api.Test
+
+class DefaultConfigResolverTest {
+  @Test
+  fun `resolves configuration`() {
+    TODO()
+  }
+}
