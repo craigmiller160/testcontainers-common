209a9e9f5c4f9375bbb063bd31a1850ed8d55dd4
Craig Miller <craigmiller160@gmail.com>
2023-01-20 16:19:16 -0500

    More work on extension

--- src/main/kotlin/io/craigmiller160/testcontainers/common/TestcontainersExtension.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/TestcontainersExtension.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.testcontainers.common
 
 import io.craigmiller160.testcontainers.common.config.DefaultConfigResolver
+import io.craigmiller160.testcontainers.common.utils.Terminal
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 
@@ -9,6 +10,16 @@ class TestcontainersExtension : BeforeAllCallback {
 
   override fun beforeAll(context: ExtensionContext) {
     val config = resolver.resolve()
+    val containers =
+      runCatching { Terminal.runCommand("docker ps") }
+        .map { getContainers(it) }
+        .getOrElse { listOf() }
     TODO("Not yet implemented")
   }
+
+  private fun getContainers(output: String): List<String> =
+    output
+      .split("\n")
+      .filter { row -> row.trim().isNotBlank() }
+      .mapNotNull { row -> Regex("\\S+$").find(row)?.value }
 }
--- src/main/kotlin/io/craigmiller160/testcontainers/common/containers/PostgresContainerWrapper.kt
+++
@@ -1,5 +0,0 @@
-package io.craigmiller160.testcontainers.common.containers
-
-import io.craigmiller160.testcontainers.common.config.ContainerConfig
-
-class PostgresContainerWrapper(private val config: ContainerConfig) {}
