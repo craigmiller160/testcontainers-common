2f7146f4078941489584523cfb787eaa17a567d5
Craig Miller <craigmiller160@gmail.com>
2023-01-20 16:23:13 -0500

    More work on extension

--- src/main/kotlin/io/craigmiller160/testcontainers/common/TestcontainersExtension.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/TestcontainersExtension.kt
@@ -1,9 +1,12 @@
 package io.craigmiller160.testcontainers.common
 
+import dasniko.testcontainers.keycloak.KeycloakContainer
+import io.craigmiller160.testcontainers.common.config.ContainerConfig
 import io.craigmiller160.testcontainers.common.config.DefaultConfigResolver
 import io.craigmiller160.testcontainers.common.utils.Terminal
 import org.junit.jupiter.api.extension.BeforeAllCallback
 import org.junit.jupiter.api.extension.ExtensionContext
+import org.testcontainers.containers.PostgreSQLContainer
 
 class TestcontainersExtension : BeforeAllCallback {
   private val resolver = DefaultConfigResolver()
@@ -16,17 +19,23 @@ class TestcontainersExtension : BeforeAllCallback {
         .getOrElse { listOf() }
 
     if (config.postgres.enable && !containers.contains(config.postgres.externalName)) {
-      startPostgresContainer()
+      startPostgresContainer(config.postgres)
     }
 
     if (config.keycloak.enable && !containers.contains(config.keycloak.externalName)) {
-      startKeycloakContainer()
+      startKeycloakContainer(config.keycloak)
     }
   }
 
-  private fun startKeycloakContainer() {}
+  private fun startKeycloakContainer(config: ContainerConfig) {
+    val imageTag = config.overrideImageTag ?: "20.0.2"
+    val container = KeycloakContainer("keycloak:$imageTag")
+  }
 
-  private fun startPostgresContainer() {}
+  private fun startPostgresContainer(config: ContainerConfig) {
+    val imageTag = config.overrideImageTag ?: "14.5"
+    val container = PostgreSQLContainer("postgres:${config.overrideImageTag}")
+  }
 
   private fun getContainers(output: String): List<String> =
     output
--- src/main/kotlin/io/craigmiller160/testcontainers/common/config/ContainerConfig.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/config/ContainerConfig.kt
@@ -1,3 +1,7 @@
 package io.craigmiller160.testcontainers.common.config
 
-data class ContainerConfig(val enable: Boolean, val externalName: String? = null)
+data class ContainerConfig(
+  val enable: Boolean,
+  val overrideImageTag: String? = null,
+  val externalName: String? = null
+)
