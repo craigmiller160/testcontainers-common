a590e5f94590d72b4f8b5b145a6f7cc858d69f09
Craig Miller <craigmiller160@gmail.com>
2023-01-22 18:56:43 -0500

    Refactored some of the config loading

--- src/main/kotlin/io/craigmiller160/testcontainers/common/config/DefaultConfigResolver.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/config/DefaultConfigResolver.kt
@@ -4,8 +4,9 @@ import org.yaml.snakeyaml.Yaml
 
 class DefaultConfigResolver : ConfigResolver {
   override fun resolve(): TestcontainersCommonConfig =
-    javaClass.classLoader.getResourceAsStream("testcontainers-common.yml")?.use { stream ->
-      Yaml().load(stream)
-    }
+    javaClass.classLoader
+      .getResourceAsStream("testcontainers-common.yml")
+      ?.use { stream -> Yaml().load<Map<String, Any>>(stream) }
+      ?.let { TestcontainersCommonConfig(it) }
       ?: throw IllegalStateException("No testcontainers-common.yml file found at root of classpath")
 }
--- src/main/kotlin/io/craigmiller160/testcontainers/common/config/TestcontainersCommonConfig.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/config/TestcontainersCommonConfig.kt
@@ -3,4 +3,6 @@ package io.craigmiller160.testcontainers.common.config
 data class TestcontainersCommonConfig(
   val postgres: ContainerConfig?,
   val keycloak: ContainerConfig?
-)
+) {
+  constructor(map: Map<String, Any>) : this(null, null)
+}
