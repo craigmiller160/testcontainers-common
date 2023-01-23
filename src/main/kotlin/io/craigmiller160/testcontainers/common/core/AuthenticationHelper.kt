39b683efe05ac09ea8808b1cd553c9db79c666a7
Craig Miller <craigmiller160@gmail.com>
2023-01-22 20:36:23 -0500

    Progress with auth helper setup

--- src/main/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelper.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelper.kt
@@ -3,8 +3,17 @@ package io.craigmiller160.testcontainers.common.core
 import org.keycloak.admin.client.KeycloakBuilder
 
 class AuthenticationHelper {
+  companion object {
+    const val ADMIN_CLIENT_ID = "admin-cli"
+    const val CLIENT_GRANT_TYPE = "password"
+  }
+
   private val keycloak =
     KeycloakBuilder.builder()
       .serverUrl(System.getProperty(TestcontainerConstants.KEYCLOAK_URL_PROP))
+      .username(TestcontainerConstants.KEYCLOAK_ADMIN_USER)
+      .password(TestcontainerConstants.KEYCLOAK_ADMIN_PASSWORD)
+      .clientId(ADMIN_CLIENT_ID)
+      .grantType(CLIENT_GRANT_TYPE)
       .build()
 }
