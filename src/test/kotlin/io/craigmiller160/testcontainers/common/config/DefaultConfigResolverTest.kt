56b813fe59708a10eacaa46fd8244831973e9609
Craig Miller <craigmiller160@gmail.com>
2023-01-22 19:02:11 -0500

    Updated tests

--- src/test/kotlin/io/craigmiller160/testcontainers/common/config/DefaultConfigResolverTest.kt
+++ src/test/kotlin/io/craigmiller160/testcontainers/common/config/DefaultConfigResolverTest.kt
@@ -5,11 +5,25 @@ import org.junit.jupiter.api.Test
 
 class DefaultConfigResolverTest {
   @Test
-  fun `resolves configuration`() {
-    // TODO need to test different scenarios
+  fun `resolves configuration for all properties`() {
     val resolver = DefaultConfigResolver()
     val config = resolver.resolve()
     assertThat(config.postgres).isNotNull.hasFieldOrPropertyWithValue("enable", true)
     assertThat(config.keycloak).isNotNull.hasFieldOrPropertyWithValue("enable", true)
+  }
+
+  @Test
+  fun `resolves configuration for postgres only`() {
+    TODO()
+  }
+
+  @Test
+  fun `resolves configuration for keycloak only`() {
+    TODO()
+  }
+
+  @Test
+  fun `resolves configuration with no configuration provided`() {
+    TODO()
   }
 }
