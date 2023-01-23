3a7dfe60e80e5660f1a68cfd92d3894f1c6b5e89
Craig Miller <craigmiller160@gmail.com>
2023-01-23 16:14:08 -0500

    More work on tests

--- src/test/kotlin/io/craigmiller160/testcontainers/common/core/TestcontainerInitializerTest.kt
+++ src/test/kotlin/io/craigmiller160/testcontainers/common/core/TestcontainerInitializerTest.kt
@@ -2,6 +2,7 @@ package io.craigmiller160.testcontainers.common.core
 
 import io.craigmiller160.testcontainers.common.config.ContainerConfig
 import io.craigmiller160.testcontainers.common.config.TestcontainersCommonConfig
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class TestcontainerInitializerTest {
@@ -10,7 +11,30 @@ class TestcontainerInitializerTest {
       postgres = ContainerConfig(enable = true), keycloak = ContainerConfig(enable = true))
 
   @Test
-  fun `can initialize containers`() {
-    TestcontainerInitializer.initialize(config)
+  fun `can initialize all containers`() {
+    val result = TestcontainerInitializer.initialize(config)
+    assertThat(result)
+      .hasFieldOrPropertyWithValue("postgres", ContainerStatus.STARTED)
+      .hasFieldOrPropertyWithValue("keycloak", ContainerStatus.STARTED)
+  }
+
+  @Test
+  fun `can initialize postgres only`() {
+    TODO()
+  }
+
+  @Test
+  fun `can initialize keycloak only`() {
+    TODO()
+  }
+
+  @Test
+  fun `can initialize nothing`() {
+    TODO()
+  }
+
+  @Test
+  fun `can initialize all containers and re-map properties`() {
+    TODO()
   }
 }
