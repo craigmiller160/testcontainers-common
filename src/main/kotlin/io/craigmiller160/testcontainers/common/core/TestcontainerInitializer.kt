c568ebb330de7d39f949ba93e0a7811339280bf7
Craig Miller <craigmiller160@gmail.com>
2023-01-23 16:29:25 -0500

    Refactored property mappings

--- src/main/kotlin/io/craigmiller160/testcontainers/common/core/TestcontainerInitializer.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/core/TestcontainerInitializer.kt
@@ -1,6 +1,7 @@
 package io.craigmiller160.testcontainers.common.core
 
 import dasniko.testcontainers.keycloak.KeycloakContainer
+import io.craigmiller160.testcontainers.common.config.ContainerConfig
 import io.craigmiller160.testcontainers.common.config.TestcontainersCommonConfig
 import org.testcontainers.containers.PostgreSQLContainer
 
@@ -8,14 +9,14 @@ object TestcontainerInitializer {
   fun initialize(config: TestcontainersCommonConfig): ContainerInitializationResult {
     val (postgresStatus, postgresContainer) =
       if (config.postgres?.enable == true) {
-        startPostgresContainer()
+        startPostgresContainer(config.postgres)
       } else {
         ContainerStatus.DISABLED to null
       }
 
     val (keycloakStatus, keycloakContainer) =
       if (config.keycloak?.enable == true) {
-        startKeycloakContainer()
+        startKeycloakContainer(config.keycloak)
       } else {
         ContainerStatus.DISABLED to null
       }
@@ -27,7 +28,9 @@ object TestcontainerInitializer {
       keycloakContainer = keycloakContainer)
   }
 
-  private fun startKeycloakContainer(): Pair<ContainerStatus, KeycloakContainer> {
+  private fun startKeycloakContainer(
+    config: ContainerConfig
+  ): Pair<ContainerStatus, KeycloakContainer> {
     val container =
       KeycloakContainer(TestcontainerConstants.KEYCLOAK_IMAGE)
         .withAdminUsername(TestcontainerConstants.KEYCLOAK_ADMIN_USER)
@@ -35,11 +38,23 @@ object TestcontainerInitializer {
         .withRealmImportFile(TestcontainerConstants.KEYCLOAK_REALM_FILE)
         .withReuse(true)
         .also { it.start() }
-    System.setProperty(TestcontainerConstants.KEYCLOAK_URL_PROP, container.authServerUrl)
+    setProperty(
+      config.propertyMappings, TestcontainerConstants.KEYCLOAK_URL_PROP, container.authServerUrl)
     return ContainerStatus.STARTED to container
   }
 
-  private fun startPostgresContainer(): Pair<ContainerStatus, PostgreSQLContainer<*>> {
+  private fun setProperty(
+    propertyMappings: Map<String, String>,
+    defaultKey: String,
+    value: String
+  ) {
+    propertyMappings[defaultKey]?.let { System.setProperty(it, value) }
+      ?: System.setProperty(defaultKey, value)
+  }
+
+  private fun startPostgresContainer(
+    config: ContainerConfig
+  ): Pair<ContainerStatus, PostgreSQLContainer<*>> {
     val container =
       PostgreSQLContainer(TestcontainerConstants.POSTGRES_IMAGE)
         .withUsername(TestcontainerConstants.POSTGRES_USER)
@@ -47,9 +62,12 @@ object TestcontainerInitializer {
         .withDatabaseName(TestcontainerConstants.POSTGRES_DB_NAME)
         .withReuse(true)
         .also { it.start() }
-    System.setProperty(TestcontainerConstants.POSTGRES_URL_PROP, container.jdbcUrl)
-    System.setProperty(TestcontainerConstants.POSTGRES_PASSWORD_PROP, container.password)
-    System.setProperty(TestcontainerConstants.POSTGRES_USER_PROP, container.username)
+    setProperty(
+      config.propertyMappings, TestcontainerConstants.POSTGRES_URL_PROP, container.jdbcUrl)
+    setProperty(
+      config.propertyMappings, TestcontainerConstants.POSTGRES_PASSWORD_PROP, container.password)
+    setProperty(
+      config.propertyMappings, TestcontainerConstants.POSTGRES_USER_PROP, container.username)
     return ContainerStatus.STARTED to container
   }
 }
