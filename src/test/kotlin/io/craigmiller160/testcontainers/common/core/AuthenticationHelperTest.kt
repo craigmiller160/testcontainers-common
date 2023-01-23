8393300a4a410c26c88bbb0e658f760579ec469c
Craig Miller <craigmiller160@gmail.com>
2023-01-23 08:34:02 -0500

    More test logic

--- src/test/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelperTest.kt
+++ src/test/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelperTest.kt
@@ -3,6 +3,7 @@ package io.craigmiller160.testcontainers.common.core
 import io.craigmiller160.testcontainers.common.config.ContainerConfig
 import io.craigmiller160.testcontainers.common.config.TestcontainersCommonConfig
 import java.util.UUID
+import kotlin.test.assertNotNull
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
@@ -25,5 +26,8 @@ class AuthenticationHelperTest {
     assertThat(testUser)
       .hasFieldOrPropertyWithValue("userName", userName)
       .hasFieldOrPropertyWithValue("roles", listOf(AuthenticationHelper.ACCESS_ROLE))
+
+    val token = helper.login(testUser)
+    assertNotNull(token)
   }
 }
