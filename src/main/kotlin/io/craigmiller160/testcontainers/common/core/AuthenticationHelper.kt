e606f83d451a3892ae7bd52c0e6e73b5fb509cab
Craig Miller <craigmiller160@gmail.com>
2023-01-22 20:40:50 -0500

    More progress

--- src/main/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelper.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelper.kt
@@ -1,11 +1,14 @@
 package io.craigmiller160.testcontainers.common.core
 
+import java.util.UUID
 import org.keycloak.admin.client.KeycloakBuilder
 
 class AuthenticationHelper {
   companion object {
     const val ADMIN_CLIENT_ID = "admin-cli"
-    const val CLIENT_GRANT_TYPE = "password"
+    const val CLI_GRANT_TYPE = "password"
+    const val CLIENT_ID = "test-client"
+    const val CLIENT_SECRET = "CQ60kzXcV7F31JfW8dX2MINjVIKOIxpJ"
   }
 
   private val keycloak =
@@ -16,4 +19,13 @@ class AuthenticationHelper {
       .clientId(ADMIN_CLIENT_ID)
       .grantType(CLIENT_GRANT_TYPE)
       .build()
+
+  fun login(userName: String, password: String): String {}
+
+  data class TestUser(
+    val userId: UUID,
+    val userName: String,
+    val roles: List<String>,
+    val token: String
+  )
 }
