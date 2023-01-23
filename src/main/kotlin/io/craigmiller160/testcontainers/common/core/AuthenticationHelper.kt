1df8b9439c06f4fa22e42127d3956db06ec1004c
Craig Miller <craigmiller160@gmail.com>
2023-01-22 20:34:25 -0500

    Beginning to setup auth helper

--- src/main/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelper.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelper.kt
@@ -3,5 +3,8 @@ package io.craigmiller160.testcontainers.common.core
 import org.keycloak.admin.client.KeycloakBuilder
 
 class AuthenticationHelper {
-  private val keycloak = KeycloakBuilder.builder().serverUrl().build()
+  private val keycloak =
+    KeycloakBuilder.builder()
+      .serverUrl(System.getProperty(TestcontainerConstants.KEYCLOAK_URL_PROP))
+      .build()
 }
