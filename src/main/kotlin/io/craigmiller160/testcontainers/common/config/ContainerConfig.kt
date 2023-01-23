224683e8e0d3d4372b5caf58e1836a664dcd80db
Craig Miller <craigmiller160@gmail.com>
2023-01-23 16:05:26 -0500

    Progress

--- src/main/kotlin/io/craigmiller160/testcontainers/common/config/ContainerConfig.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/config/ContainerConfig.kt
@@ -1,6 +1,9 @@
 package io.craigmiller160.testcontainers.common.config
 
-data class ContainerConfig(val enable: Boolean, val propertyMappings: Map<String, String>) {
+data class ContainerConfig(
+  val enable: Boolean,
+  val propertyMappings: Map<String, String> = mapOf()
+) {
   constructor(
     map: Map<String, Any>
   ) : this(map["enable"] as Boolean, map["propertyMappings"] as Map<String, String>? ?: mapOf())
