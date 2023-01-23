7f7788822e6535bbe7acb23b4a1d1cca59ef6e10
Craig Miller <craigmiller160@gmail.com>
2023-01-23 08:26:19 -0500

    Test setup work

--- src/test/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelperTest.kt
+++ src/test/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelperTest.kt
@@ -1,8 +1,19 @@
 package io.craigmiller160.testcontainers.common.core
 
+import io.craigmiller160.testcontainers.common.config.ContainerConfig
+import io.craigmiller160.testcontainers.common.config.TestcontainersCommonConfig
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
 class AuthenticationHelperTest {
+  companion object {
+    @BeforeAll
+    @JvmStatic
+    fun setup() {
+      TestcontainerInitializer.initialize(
+        TestcontainersCommonConfig(postgres = null, keycloak = ContainerConfig(enable = true)))
+    }
+  }
   @Test
   fun `can create a user in keycloak and then login with that user`() {
     TODO()
