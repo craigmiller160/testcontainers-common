82416567fd6fcaddd6374a7021e469b87df46137
Craig Miller <craigmiller160@gmail.com>
2023-01-22 19:03:25 -0500

    Implemented more tests

--- src/test/kotlin/io/craigmiller160/testcontainers/common/config/DefaultConfigResolverTest.kt
+++ src/test/kotlin/io/craigmiller160/testcontainers/common/config/DefaultConfigResolverTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class DefaultConfigResolverTest {
   @Test
   fun `resolves configuration for all properties`() {
-    val resolver = DefaultConfigResolver()
+    val resolver = DefaultConfigResolver("configResolverTest/all-props.yml")
     val config = resolver.resolve()
     assertThat(config.postgres).isNotNull.hasFieldOrPropertyWithValue("enable", true)
     assertThat(config.keycloak).isNotNull.hasFieldOrPropertyWithValue("enable", true)
@@ -14,12 +14,18 @@ class DefaultConfigResolverTest {
 
   @Test
   fun `resolves configuration for postgres only`() {
-    TODO()
+    val resolver = DefaultConfigResolver("configResolverTest/postgres-only.yml")
+    val config = resolver.resolve()
+    assertThat(config.postgres).isNotNull.hasFieldOrPropertyWithValue("enable", true)
+    assertThat(config.keycloak).isNull()
   }
 
   @Test
   fun `resolves configuration for keycloak only`() {
-    TODO()
+    val resolver = DefaultConfigResolver("configResolverTest/keycloak-only.yml")
+    val config = resolver.resolve()
+    assertThat(config.postgres).isNull()
+    assertThat(config.keycloak).isNotNull.hasFieldOrPropertyWithValue("enable", true)
   }
 
   @Test
