271a2f7a2e247d102adff9394c0b6fbfb1b86446
Craig Miller <craigmiller160@gmail.com>
2023-01-24 15:48:14 -0500

    Added back Terminal utility

---
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/utils/Terminal.kt
@@ -0,0 +1,18 @@
+package io.craigmiller160.testcontainers.common.utils
+
+import java.io.File
+import java.util.concurrent.TimeUnit
+
+object Terminal {
+  fun execute(command: String): String {
+    val processBuilder =
+      ProcessBuilder("\\s".toRegex().split(command))
+        .directory(File("."))
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .redirectError(ProcessBuilder.Redirect.PIPE)
+    return runCatching { processBuilder.start() }
+      .mapCatching { process -> process.also { process.waitFor(60, TimeUnit.SECONDS) } }
+      .mapCatching { process -> process.inputStream.bufferedReader().readText() }
+      .getOrThrow()
+  }
+}
