5dc72c90477a2317297fa391c362fbd3ab607b43
Craig Miller <craigmiller160@gmail.com>
2023-01-25 12:37:25 -0500

    Updated test

--- src/test/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelperTest.kt
+++ src/test/kotlin/io/craigmiller160/testcontainers/common/core/AuthenticationHelperTest.kt
@@ -2,7 +2,6 @@ package io.craigmiller160.testcontainers.common.core
 
 import io.craigmiller160.testcontainers.common.config.ContainerConfig
 import io.craigmiller160.testcontainers.common.config.TestcontainersCommonConfig
-import java.util.UUID
 import kotlin.test.assertNotNull
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
@@ -20,12 +19,11 @@ class AuthenticationHelperTest {
   @Test
   fun `can create a user in keycloak and then login with that user`() {
     val helper = AuthenticationHelper()
-    val id = UUID.randomUUID().toString()
-    val userName = "MyUser_$id@gmail.com"
+    val userName = "MyUser@gmail.com"
     val testUser = helper.createUser(userName)
     assertThat(testUser)
-      .hasFieldOrPropertyWithValue("userName", userName)
       .hasFieldOrPropertyWithValue("roles", listOf(AuthenticationHelper.ACCESS_ROLE))
+    assertThat(testUser.userName).matches("^\\S+_$userName")
 
     val token = helper.login(testUser)
     assertNotNull(token)
