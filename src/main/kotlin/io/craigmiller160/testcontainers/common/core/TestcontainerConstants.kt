64758f61253297e89cfa4a65e95687d6fd5e9233
Craig Miller <craigmiller160@gmail.com>
2023-01-25 10:12:52 -0500

    Updated property keys with new prefix, removed mappings from config

--- src/main/kotlin/io/craigmiller160/testcontainers/common/core/TestcontainerConstants.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/core/TestcontainerConstants.kt
@@ -1,16 +1,15 @@
 package io.craigmiller160.testcontainers.common.core
 
 object TestcontainerConstants {
-  // TODO add prefix to properties
   // TODO add keycloak user & password as properties
   const val KEYCLOAK_IMAGE = "quay.io/keycloak/keycloak:20.0.2"
   const val KEYCLOAK_REALM_FILE = "keycloak-realm.json"
   const val KEYCLOAK_ADMIN_USER = "admin"
   const val KEYCLOAK_ADMIN_PASSWORD = "password"
-  const val KEYCLOAK_URL_PROP = "keycloak.url"
-  const val KEYCLOAK_REALM_PROP = "keycloak.realm"
-  const val KEYCLOAK_CLIENT_ID_PROP = "keycloak.client.id"
-  const val KEYCLOAK_CLIENT_SECRET_PROP = "keycloak.client.secret"
+  const val KEYCLOAK_URL_PROP = "testcontainers.common.keycloak.url"
+  const val KEYCLOAK_REALM_PROP = "testcontainers.common.keycloak.realm"
+  const val KEYCLOAK_CLIENT_ID_PROP = "testcontainers.common.keycloak.client.id"
+  const val KEYCLOAK_CLIENT_SECRET_PROP = "testcontainers.common.keycloak.client.secret"
   const val KEYCLOAK_REALM = "apps-dev"
   const val KEYCLOAK_CLIENT_ID = "test-client"
   const val KEYCLOAK_CLIENT_SECRET = "CQ60kzXcV7F31JfW8dX2MINjVIKOIxpJ"
@@ -19,8 +18,8 @@ object TestcontainerConstants {
   const val POSTGRES_USER = "user"
   const val POSTGRES_PASSWORD = "password"
   const val POSTGRES_DB_NAME = "test"
-  const val POSTGRES_URL_PROP = "postgres.url"
-  const val POSTGRES_USER_PROP = "postgres.username"
-  const val POSTGRES_PASSWORD_PROP = "postgres.password"
-  const val POSTGRES_SCHEMA_PROP = "postgres.schema"
+  const val POSTGRES_URL_PROP = "testcontainers.common.postgres.url"
+  const val POSTGRES_USER_PROP = "testcontainers.common.postgres.username"
+  const val POSTGRES_PASSWORD_PROP = "testcontainers.common.postgres.password"
+  const val POSTGRES_SCHEMA_PROP = "testcontainers.common.postgres.schema"
 }
