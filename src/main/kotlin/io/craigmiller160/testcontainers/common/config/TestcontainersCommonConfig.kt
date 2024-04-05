fd3a7d964594ddc23172f76fe692ff20bc49c9bf
Craig Miller <craigmiller160@gmail.com>
2024-04-05 08:38:57 -0400

    Added mongo

--- src/main/kotlin/io/craigmiller160/testcontainers/common/config/TestcontainersCommonConfig.kt
+++ src/main/kotlin/io/craigmiller160/testcontainers/common/config/TestcontainersCommonConfig.kt
@@ -2,11 +2,13 @@ package io.craigmiller160.testcontainers.common.config
 
 data class TestcontainersCommonConfig(
   val postgres: ContainerConfig?,
-  val keycloak: ContainerConfig?
+  val keycloak: ContainerConfig?,
+  val mongo: ContainerConfig?
 ) {
   constructor(
     map: Map<String, Any>
   ) : this(
     map["postgres"]?.let { ContainerConfig(it as Map<String, Any>) },
-    map["keycloak"]?.let { ContainerConfig(it as Map<String, Any>) })
+    map["keycloak"]?.let { ContainerConfig(it as Map<String, Any>) },
+    map["mongo"]?.let { ContainerConfig(it as Map<String, Any>) })
 }
